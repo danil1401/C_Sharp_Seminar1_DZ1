9a878fd37965da0c5c1bf5f236482ae33b67ff76
danil1401 <danil1401@yandex.ru>
2022-10-06 20:53:17 +0400

    Начало решения Zd8

--- ZD8/Program.cs
+++ ZD8/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.Clear();
+
+int N;
+int i=1;
+Console.WriteLine("Введите число N: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+    if (N % 2 == 0)
+        Console.WriteLine(N);
