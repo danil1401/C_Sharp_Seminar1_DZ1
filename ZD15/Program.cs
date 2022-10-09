e56ff0d1da5dd61dcb546f231cf8fd4f2c165f97
danil1401 <danil1401@yandex.ru>
2022-10-09 23:03:54 +0400

    ЗД15 Решение Каждую пятницу я...Огурцом!

---
+++ ZD15/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 15. Написать программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+/*Console.WriteLine("Введите номер от 1 до 7: ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (n==6| n==7)
+{
+    Console.WriteLine ("Сегодня Выходной ");
+}
+
+if (n==5)
+{
+    Console.WriteLine ("Каждую пятнциу я в ...)))");
+}
+else
+{
+    Console.WriteLine ("Выходные не за горами ;)");
+}*/
+
+Console.WriteLine("Введите номер от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7) // зацикливание для корректного ввода значения от 1го до 7ми
+    {
+        Console.WriteLine("Неделя состоит из 7ми дней, повторите ввод");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+if (n == 6 || n == 7)
+    {
+    Console.WriteLine("Выходные-проведи их с пользой!");
+    }
+
+else Console.WriteLine("Рабочие будни. Выходные близко");
