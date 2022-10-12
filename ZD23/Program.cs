b8efb66cbf580a6947417a57424ec89060865373
danil1401 <danil1401@yandex.ru>
2022-10-13 00:43:50 +0400

    Решение Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.

--- ZD23/Program.cs
+++ ZD23/Program.cs
@@ -5,4 +5,10 @@ Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 0; i <= n; i++)
-Console.Write($" {i * i * i} ");
+Console.WriteLine($" {i * i * i} ");
+
+
+/*Только куб введеного числа
+Console.Write("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write($" {i * i * i} ");*/
