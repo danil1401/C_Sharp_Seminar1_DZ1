550f4fa84bf08480d75717f9f36465029cfbaee5
danil1401 <danil1401@yandex.ru>
2022-11-13 17:52:55 +0400

    Решение ЗД62Спираль

---
+++ ZD62/Program.cs
@@ -0,0 +1,42 @@
+﻿/*  Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07   */
+
+Console.Clear();
+Console.Write("Введите кол-во строк или столбцов квадратного массива : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+int[,] matrix = new int[n, m];
+Console.WriteLine($"Спирально заполненный массив: ");
+InputMatrix(matrix);
+
+void InputMatrix(int[,] matrix)
+{
+    int temp = 1, i = 0, j = 0;
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+
+    }
+    for (i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+
+}
+
+
