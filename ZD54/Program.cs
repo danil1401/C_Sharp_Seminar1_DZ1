ae609b3192aecf966d7a8b6594a4e49d9bf053e4
danil1401 <danil1401@yandex.ru>
2022-11-13 17:44:33 +0400

    Решешие ЗД54

---
+++ ZD54/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+Console.WriteLine($"Начальный массив: ");
+InputMatrix(matrix);
+WriteMatrix(matrix);
+Console.WriteLine($"Конечный массив: ");
+ReleaseMatrix(matrix);
+WriteMatrix(matrix);
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void WriteMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int a = 0; a < matrix.GetLength(1) - 1; a++)
+            {
+                if (matrix[i, a] < matrix[i, a + 1])
+                {
+                    int temp = matrix[i, a + 1];
+                    matrix[i, a + 1] = matrix[i, a];
+                    matrix[i, a] = temp;
+                }
+            }
+        }
+    }
+}
+
